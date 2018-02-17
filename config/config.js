9253a4cfe2b682f532bb62977f771a39a61dab16
DGBeladiya09 <dharmik.beladiya11231@marwadieducation.edu.in>
2018-02-17 20:47:49 +0530

    Database shift to MongoDB

--- config/config.js
+++ config/config.js
@@ -1,4 +1,5 @@
 module.exports = {
-    dbURL: "mongodb://localhost:27017/Practise",
+    dbURL: "mongodb://project:project@ds161162.mlab.com:61162/testfooddb",
     mode: "Development"
+    //mongodb://localhost:27017/Practise
 }
