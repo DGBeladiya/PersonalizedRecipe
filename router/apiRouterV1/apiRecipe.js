14e12954fc21f016402d69d8d00fc776f97fb155
DGBeladiya09 <36558238+DGBeladiya09@users.noreply.github.com>
2018-03-19 14:24:06 +0000

    Update apiRecipe.js

--- router/apiRouterV1/apiRecipe.js
+++ router/apiRouterV1/apiRecipe.js
@@ -15,7 +15,7 @@ function Response() {
 router.post("/ListbyIngredient", jsonParser,(req, res) => {
   var ingrdeint=req.body.ingrdeint
   console.log(ingrdeint)
-    recipe.find({"ingredients.name":{$in:ingrdeint}} ,"name image category",
+    recipe.find({"ingredients.name":{$in:["Brinjal"]}} ,"name image category",
      (err, docs) => {
         res.send(docs)
     })
