0d68ae0df89550fa3e552c31e06470861b321f68
DGBeladiya09 <dharmik.beladiya11231@marwadieducation.edu.in>
2018-03-21 23:57:31 +0530

    Api recipe Update

--- router/apiRouterV1/apiRecipe.js
+++ router/apiRouterV1/apiRecipe.js
@@ -1,9 +1,9 @@
 var express = require("express")
 var router = express.Router();
 var recipe = require("../../model/recipe.js")
-var bodyParser=require("body-parser")
+var bodyParser = require("body-parser")
 var jsonParser = bodyParser.json();
-var urlEncoded=bodyParser.urlencoded({extended: false})
+var urlEncoded = bodyParser.urlencoded({ extended: false })
 function Response() {
     this.status = "";
     this.errors = {};
@@ -11,25 +11,27 @@ function Response() {
     this.data = {},
         this.statusCode = 0;
 }
-
-router.post("/ListbyIngredient", jsonParser,(req, res) => {
-  var data=req.body.ingrdeint
-
- 
-    
-    recipe.find({"ingredients.name":{$in:data}} ,"name image category time",
-     (err, docs) => {
-        res.send(docs)
-    })
+router.post("/ListbyIngredient", jsonParser, (req, res) => {
+    var data = req.body.ingrdeint
+   
+        recipe.aggregate([{$match:{"ingredients.name":{$in:data}}},
+            { $project: { name: true, image: true, category: true, time: true, rating: { $avg: "$review.rating" } } }
+        ], (err, docs) => {
+            res.send(docs)
+        });
 })
+router.post("/ListbyCategory", jsonParser, (req, res) => {
+    var data = req.body.category
 
-router.post("/ListbyCategory", jsonParser,(req, res) => {
-  var data=req.body.category
-
- 
-    
-    recipe.find({category:data} ,"name image category time",
-     (err, docs) => {
+    recipe.aggregate([{$match:{"category":data}},
+        { $project: { name: true, image: true, category: true, time: true, rating: { $avg: "$review.rating" } } }
+    ], (err, docs) => {
+        res.send(docs)
+    });
+})
+router.post("/GetRecipeInformation", jsonParser, (req, res) => {
+    var _id = req.body._id;
+    recipe.findById({ "_id": _id }, (err, docs) => {
         res.send(docs)
     })
 })
